c58da3a61f759146d07dafc4d7098b3b54495dde
mauriciovancine <mauricio.vancine@gmail.com>
2020-06-24 23:02:25 -0300

    2020-06-24 add data and change script 'script_landscape_metrics.R' to calculate metrics for multiple buffers

--- 00_scripts/script_landscape_metrics.R
+++ 00_scripts/script_landscape_metrics.R
@@ -1,7 +1,7 @@
 #' ---
 #' title: landscape metrics
 #' author: mauricio vancine
-#' date: 2020-06-23
+#' date: 2020-06-24
 #' ---
 
 # prepare r -------------------------------------------------------------
@@ -18,55 +18,32 @@ library(tidyverse)
 library(tmap)
 
 # directory
-path <- "/home/mude/data/onedrive/manuscritos/03_preparacao/lucas/02_data"
+path <- "/home/mude/data/github/world-landscape-metrics"
 setwd(path)
 dir()
 
 # import data -------------------------------------------------------------
 # raster
-ra <- raster::raster("flo.tif") > .9
+ra <- raster::raster("01_data/flo.tif") > .9
 ra
 
 # map
-tm_shape(ra) +
-  tm_raster()
+plot(ra)
 
 # import points
-po <- sf::st_read("amostra_pontos_neotropicos.shp", quiet = TRUE)
+po <- sf::st_read("01_data/amostra_pontos_neotropicos.shp", quiet = TRUE)
 po
 
 tm_shape(po) +
   tm_bubbles()
 
 # import utm grids
-utm <- sf::st_read("utm_zones_final.shp") %>% 
-  dplyr::rename(id = CATID, 
-                lon = LON, 
-                lat = LAT,
-                code = CODE) %>% 
-  dplyr::mutate(zone = stringr::str_extract(code, "[0-9]+"),
-                prj4 = dplyr::if_else(lat < 0, 
-                                      paste0("+proj=utm +zone=", zone, " +south +datum=WGS84 +units=m +no_defs +type=crs"),
-                                      paste0("+proj=utm +zone=", zone, " +datum=WGS84 +units=m +no_defs +type=crs")))
-utm
+utm_epsg <- sf::st_read("01_data/utm_zones_epsg.shp")
+utm_epsg
 
-tm_shape(utm) +
-  tm_polygons()
-
-# epsg
-epsg <- rgdal::make_EPSG() %>% 
-  tibble::as_tibble() %>% 
-  dplyr::rename(epsg_code = code) %>% 
-  dplyr::filter(str_detect(note, "WGS 84 / UTM zone"))
-epsg
-
-# join
-utm_epsg <- utm %>% 
-  dplyr::left_join(epsg, by = "prj4")
-utm_epsg  
-
-# export
-sf::st_write(utm_epsg, "utm_zones_final_epsg.shp", append = FALSE)
+tm_shape(utm_epsg) +
+  tm_polygons() +
+  tm_text("zone", size = .7)
 
 # metrics -----------------------------------------------------------------
 # metrics
@@ -83,9 +60,14 @@ for(i in 1:nrow(po)){
     dplyr::slice(i) %>% 
     sf::st_join(utm_epsg)
     
-  # transform and buffer
-  po_i_tb <- sf::st_transform(po_i, crs = po_i$epsg_code) %>% 
-    sf::st_buffer(dist = 1000)
+  for(j in seq(100, 2800, 300)){
+  
+    # info
+    print(paste0("Buffer ", j, " m"))
+    
+    # transform and buffer
+    po_i_tb <- sf::st_transform(po_i, crs = po_i$epsg_code) %>% 
+    sf::st_buffer(dist = j)
   
   # crop and mask
   ra_i <- ra %>%
@@ -107,10 +89,12 @@ for(i in 1:nrow(po)){
   
   # combine
   metrics <- dplyr::bind_rows(pl, pd, ed, si) %>%
-    dplyr::mutate(id = i) %>% 
-    dplyr::select(id, class, metric, value) %>% 
+    dplyr::mutate(id = i, buffer = j) %>% 
+    dplyr::select(id, buffer, class, metric, value) %>% 
     dplyr::bind_rows(metrics)
-  
+
+    }
+
 }
 
 # end ---------------------------------------------------------------------
