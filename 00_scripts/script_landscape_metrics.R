665842b56ce2a54854f72137011ca7b52b2127db
mauriciovancine <mauricio.vancine@gmail.com>
2020-06-27 16:57:32 -0300

    2020-06-27 script landscapemetrics

--- 00_scripts/script_landscape_metrics.R
+++ 00_scripts/script_landscape_metrics.R
@@ -1,7 +1,7 @@
 #' ---
 #' title: landscape metrics
 #' author: mauricio vancine
-#' date: 2020-06-24
+#' date: 2020-06-27
 #' ---
 
 # prepare r -------------------------------------------------------------
@@ -12,38 +12,60 @@ rm(list = ls())
 library(raster)
 library(sf)
 library(rgdal)
+library(rnaturalearth)
 library(landscapemetrics)
 library(landscapetools)
 library(tidyverse)
 library(tmap)
 
+# raster options
+raster::rasterOptions(maxmemory = 1e+200, chunksize = 1e+200)
+raster::beginCluster(n = parallel::detectCores() - 1)
+
 # directory
-path <- "/home/mude/data/github/world-landscape-metrics"
-setwd(path)
-dir()
+setwd("/home/mude/data/github/world-landscape-metrics/01_data")
 
 # import data -------------------------------------------------------------
-# raster
-ra <- raster::raster("01_data/flo.tif") > .9
-ra
+# limits
+li <- rnaturalearth::countries110 %>% 
+  sf::st_as_sf()
+li
+
+# directory
+setwd("forest_maps_cut_by_buffer")
+
+# list files
+fi <- dir(pattern = "deforestation_threshold70_binary_cgs_wgs84.tif")
+fi
+
+# landscapes
+la <- purrr::map(fi, raster::brick)
+la
 
 # map
-plot(ra)
+plot(la[[1]])
 
 # import points
-po <- sf::st_read("01_data/amostra_pontos_neotropicos.shp", quiet = TRUE)
+setwd("..")
+po <- sf::st_read("community_locations/comm_data_neotro_checked_2020_d11_06.shp", quiet = TRUE)
 po
 
-tm_shape(po) +
-  tm_bubbles()
+tm_shape(li, bbox = po) +
+  tm_polygons() +
+  tm_shape(po) +
+  tm_bubbles(size = .1, col = "red")
 
 # import utm grids
-utm_epsg <- sf::st_read("01_data/utm_zones_epsg.shp")
+utm_epsg <- sf::st_read("other_vector_data/utm_zones_epsg.shp")
 utm_epsg
 
-tm_shape(utm_epsg) +
+tm_shape(li, bbox = po) +
   tm_polygons() +
-  tm_text("zone", size = .7)
+  tm_shape(utm_epsg, bbox = po) +
+  tm_borders() +
+  tm_text("zone", size = .5) +
+  tm_shape(po) +
+  tm_bubbles(size = .1, col = "red")
 
 # metrics -----------------------------------------------------------------
 # metrics
@@ -51,7 +73,7 @@ metrics <- NULL
 
 # for
 for(i in 1:nrow(po)){
-
+  
   # info
   print(i)
   
@@ -59,42 +81,51 @@ for(i in 1:nrow(po)){
   po_i <- po %>% 
     dplyr::slice(i) %>% 
     sf::st_join(utm_epsg)
-    
-  for(j in seq(100, 2800, 300)){
   
+  # project landscape
+  la_i <- la[[i]] %>%
+    raster::projectRaster(crs = po_i$prj4, res = 30)
+  
+  for(j in seq(100, 2800, 300)){
+    
     # info
     print(paste0("Buffer ", j, " m"))
     
     # transform and buffer
-    po_i_tb <- sf::st_transform(po_i, crs = po_i$epsg_code) %>% 
-    sf::st_buffer(dist = j)
+    po_i_j <- po_i %>% 
+      sf::st_transform(crs = po_i$epsg_code) %>% 
+      sf::st_buffer(dist = j)
+    
+    # crop and mask
+    la_i_j <- la_i %>%
+      raster::crop(po_i_j) %>% 
+      raster::mask(po_i_j)
+    
+    # 1. percentage of landscape
+    pl <- landscapemetrics::lsm_c_pland(la_i_j)
+    
+    # 2. patch density
+    pd <-  landscapemetrics::lsm_c_pd(la_i_j)
+    
+    # 3. edge density
+    ed <- landscapemetrics::lsm_l_ed(la_i_j)
+    
+    # 4. splitting index
+    si <- landscapemetrics::lsm_c_split(la_i_j)
+    
+    # combine
+    metrics <- dplyr::bind_rows(pl, pd, ed, si) %>%
+      dplyr::mutate(id = i, buffer = j) %>% 
+      dplyr::select(id, buffer, class, metric, value) %>% 
+      dplyr::bind_rows(metrics)
+    
+  }
   
-  # crop and mask
-  ra_i <- ra %>%
-    raster::projectRaster(crs = po_i$prj4) %>% 
-    raster::crop(po_i_tb) %>% 
-    raster::mask(po_i_tb)
-  
-  # 1. percentage of landscape
-  pl <- landscapemetrics::lsm_c_pland(ra_i)
-
-  # 2. patch density
-  pd <-  landscapemetrics::lsm_c_pd(ra_i)
-  
-  # 3. edge density
-  ed <- landscapemetrics::lsm_c_ed(ra_i)
-  
-  # 4. splitting index
-  si <- landscapemetrics::lsm_c_split(ra_i)
-  
-  # combine
-  metrics <- dplyr::bind_rows(pl, pd, ed, si) %>%
-    dplyr::mutate(id = i, buffer = j) %>% 
-    dplyr::select(id, buffer, class, metric, value) %>% 
-    dplyr::bind_rows(metrics)
-
-    }
-
 }
+
+# export
+dir.create("02_metrics")
+
+readr::write_csv(metrics, "metrics.csv")
 
 # end ---------------------------------------------------------------------
